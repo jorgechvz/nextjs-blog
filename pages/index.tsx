93ff2a5087a5c60ed2a8b029d8a6d13a8d629f01
jorgechvz <jorgechvzpon@gmail.com>
2023-11-22 01:34:32 -0500

    Update Error in Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date"
+import { PostHomeType } from "../lib/types";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -14,7 +15,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: {allPostsData: PostHomeType[]}) {
   return (
     <Layout home>
       <Head>
